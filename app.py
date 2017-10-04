6a636c4e3d1abd9412cadf0d0fe6ae2f4c1853e8
Sergio de Leon <sgoadlr@gmail.com>
2017-10-03 22:38:05 -0500

    amazon scraping

--- app.py
+++ app.py
@@ -1,35 +1,25 @@
 
-from flask import Flask, jsonify, request
-import requests, bs4
-app = Flask(__name__)
+#from flask import Flask, jsonify, request
+import requests, bs4, sys
 
+res = requests.get('https://www.amazon.com.mx/gp/product/B01IX9Q4WM/')
+print(res.text)
+try:
+	res.raise_for_status()
+except:
+	print("error")
 
-@app.route('/')
-def scrap():
-	print('req')
-	url = request.args.get('url','0',type = str)
-	res = requests.get(url)
-	try:
-		res.raise_for_status()
-	except:
-		return jsonify(name = None, price = None, thumbnail = None)
-
-	amSoup = bs4.BeautifulSoup(res.text)
-	pictures = amSoup.select('#landingImage')
-
-	if len(pictures) == 0:
-		return jsonify(name = None, price = None, thumbnail = None)
+amSoup = bs4.BeautifulSoup(res.text)
+pictures = amSoup.select('#landingImage')
+if len(pictures) == 0:
+	print("error1")
+else:
+	sThumbnail = pictures[0].get('data-old-hires')
+	sName = pictures[0].get('alt')
+	priceBlock = amSoup.select('#priceblock_ourprice')
+	
+	if len(priceBlock) == 0:
+		print("error2")
 	else:
-		sThumbnail = pictures[0].get('data-old-hires')
-		sName = pictures[0].get('alt')
-		priceBlock = amSoup.select('#priceblock_ourprice')
-		
-		if len(priceBlock) == 0:
-			return jsonify(name = None, price = None, thumbnail = None)
-		else:
-			sPrice = priceBlock[0].getText()
-
-	return jsonify(name = sName, price = sPrice, thumbnail = sThumbnail)
-
-if __name__ == "__main__":
-	app.run()
+		sPrice = priceBlock[0].getText()
+		print (sName + " " + sPrice +" " + sThumbnail)
