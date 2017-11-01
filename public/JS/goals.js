9929ea83f3603077090146a2d4590a3a8700b974
rogelioVG <rogelio.vg@icloud.com>
2017-10-31 19:57:51 -0600

    settings complete

--- public/JS/goals.js
+++ public/JS/goals.js
@@ -175,11 +175,7 @@ function loadGoals() {
         html += "<tr  id = '" + key + "'> <td><img class='thumbnail' src ='" + sThumbnail + "''></td>" 
         + " <td> "
         + " <div> <button class='delete-button'> x </button> </div> "
-<<<<<<< HEAD
-        + " <p class='product-name'>" + sName + "</p>" 
-        + " <div class='percentage'> "+((Number(sNest.substring(1)) * 100) / Number(sPrice.substring(1))).toPrecision(2) + "%</div>" 
-=======
->>>>>>> 8a26e3340bef9a5984144c1682488c1ef74279ee
+
         + "<p class='product-name'>" + sName + "</p>" 
         + " <div class='percentage'> "+ Math.floor((Number(sNest.substring(1)) * 100) / Number(sPrice.substring(1))) + "%</div>" 
         + " <div class='right'> "
